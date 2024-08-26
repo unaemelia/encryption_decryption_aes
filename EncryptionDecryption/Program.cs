9c206a31b1c4cfae11039fdbab09ad0735dbfb72
Una Árnadóttir <unaemelia@gmail.com>
2024-08-26 14:26:05 +0200

    Add project files.

---
+++ EncryptionDecryption/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EncryptionDecryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+            Console.WriteLine("Enter a string for encryption");
+            var str = Console.ReadLine();
+            var encryptedString = AesOperation.EnryptionString(key, str);
+            Console.WriteLine($"Encrypted string: {encryptedString}");
+
+            var decryptedString = AesOperation.DecryptString(key, encryptedString);
+            Console.WriteLine($"Decrypted string: {decryptedString}");
+
+            Console.ReadKey();
+        }
+    }
+}
